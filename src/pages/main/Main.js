04625f18041c6d12559f558ee21e91e6e7265253
Rhqtn <rjsdnsla159@naver.com>
2024-05-22 15:14:34 +0900

    Main.js 메인페이지
    이미지 추가 및 텍스트 입력, 배경 추가

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import catTower from "./catTower.png";
+import defaultProfile from "./defaultProfile.png";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const Main = () => {
+export default function Main() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/inputInfo");
+  };
+
   return (
-    <div>
-      메인페이지
+    <div id="MainWrapper">
+      <Link id="defaultProfile" to="/profile">
+        <img src={defaultProfile} alt="profile" />
+      </Link>
+      <div id="introTextWrapper">
+        <p id="introText">
+          “캣치헬스는 반려동물에게 흔히 발생하는 ㅇㅇㅇ,xxx등의 질병을<br></br> 사진 한장의 업로드를 통하여 신속히 진단하고,<br></br> 해당 질병의 예상되는 진료비와 바로 접근 가능한 병원의 정보를<br></br> 제공하는 서비스입니다.<br></br> 불필요한 병원
+          내원으로 인한 비용 발생을 줄이고 효과적으로 진단받아<br></br> 내가 아끼는 동물이 더이상 고통받지 않게 해주세요 !”
+        </p>
+      </div>
+      <button onclick={handleClick} id="startButton">
+        시작하기
+      </button>
+      <img src={catTower} alt="catTower" id="catTower" />
     </div>
   );
-};
-
-export default Main;
+}
