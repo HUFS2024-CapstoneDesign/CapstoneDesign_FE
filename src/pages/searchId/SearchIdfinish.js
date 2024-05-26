4160d631c42b6b4f77a2a23a6b8116f5b1799547
Hyein <snhyein2001@gmail.com>
2024-05-26 19:51:32 +0900

    db 작업 전 아이디 찾기

--- src/pages/searchId/SearchIdfinish.js
+++ src/pages/searchId/SearchIdfinish.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import S from './style.js'
+import S from './style.js';
+import { NavLink } from 'react-router-dom';
 
 const SearchIdfinish = () => {
   return (
     <S.Background>
-      아이디 찾기 결과
+      <S.H1>아이디 찾기</S.H1>
+      <S.IdSearchContainer>
+        <S.ResultP>아이디 찾기 결과</S.ResultP>
+        <S.P>abc****</S.P>
+        <S.SubP>개인정보보호를 위해 아이디 뒷자리는 ***로 표시됩니다.</S.SubP>
+      </S.IdSearchContainer>
+      <NavLink to={"/login"}>
+          <S.LoginButton>로그인하러 가기</S.LoginButton>
+      </NavLink>
     </S.Background>
   );
 };
