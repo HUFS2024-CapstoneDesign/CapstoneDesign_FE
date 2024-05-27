87405515ac5eb6c1708e038a81d86159908d8988
Hyein <snhyein2001@gmail.com>
2024-05-27 10:41:29 +0900

    로그인 카카오 구현 전

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import S from './style.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+
 
 const Login = () => {
+
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  const togglePasswordVisiblity = () => {
+    setIsPasswordShown(!isPasswordShown);
+  };
   return (
-    <div>
-      로그인
-    </div>
+    <S.Background>
+      <S.H1>로그인</S.H1>
+      <S.Input type='text' placeholder='아이디를 입력해주세요'/>
+      <S.InputContainer>
+        <S.PasswordContainer>
+          <S.Input type={isPasswordShown ? 'text' : 'password'} placeholder='새로운 비밀번호 입력'/>
+              <S.Icon onClick={togglePasswordVisiblity}>
+                <FontAwesomeIcon icon={isPasswordShown ? faEyeSlash : faEye} style={{ fontSize: "18px" }} color='#9F9F9F'/>
+              </S.Icon>
+        </S.PasswordContainer>
+      </S.InputContainer>
+
+      <S.LinksContainer>
+        <NavLink to={'/searchid'}><p>아이디 찾기</p></NavLink>
+        <NavLink to={'/identification'}><p>비밀번호 찾기</p></NavLink>
+        <NavLink to={'/signup'}><p>회원가입</p></NavLink>
+      </S.LinksContainer>
+
+      <S.SubmitButton>로그인하기</S.SubmitButton>
+
+    </S.Background>
   );
 };
 
---
+++ src/pages/login/style.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+const S = {};
+
+S.Background = styled.div`
+  width: 90%;
+  height: 100vh;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+S.H1 = styled.h1`
+  font-size: ${({ theme }) => theme.FONT_SIZE["h1"]};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT["bold"]};
+  color: #000000;
+  margin-top: 10%;
+  margin-bottom: 5%;
+  @media (max-width: 727px) {
+    margin-top: 25%;
+  }
+`;
+
+S.Input = styled.input`
+  width: 500px;
+  height: 60px;
+  border: solid 2px #000000;
+  border-radius: 50px;
+  padding: 0 10px;
+  @media (max-width: 727px) {
+    width : 90%;
+    margin-top: 10%;
+  }`;
+
+S.PasswordContainer = styled.div`
+  position: relative;
+  box-sizing: border-box;
+`;
+
+S.InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+`;
+
+
+S.Icon = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 5%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`;
+
+S.LinksContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1%;
+  margin-top: 1%;
+  margin-left: 10%;
+& p{
+  font-size: 16px;
+  color: #000;
+  margin: 0 10px;
+}
+`
+S.SubmitButton = styled.button`
+  width: 240px;
+  height: 60px;
+  background-color: ${({ theme }) => theme.PALETTE.primary["main_deep"]};
+  color: #fff;
+  font-size: ${({ theme }) => theme.FONT_SIZE["default"]};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT["semibold"]};
+  border: none;
+  border-radius: 50px;
+  margin-top: 3%;
+  cursor: pointer;
+`;
+
+
+export default S;
