5e375be603d7a3284dd6881b2e4e6ed4dbfc68ce
Rhqtn <rjsdnsla159@naver.com>
2024-05-28 18:48:58 +0900

    Add averageCost button at HospitalRecommend.js

--- src/pages/result/HosipitalRecommend.js
+++ src/pages/result/HosipitalRecommend.js
@@ -1,6 +1,26 @@
 import React from "react";
 import defaultMap from "./defaultMap.png";
+import diseasesData from "./diseasesInfo.json";
+import { useState } from "react";
+
 const HosipitalRecommend = () => {
+  const [disease, setDisease] = useState("결막염");
+  const [cost, setCost] = useState("");
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
+
+  const getDiseaseInfo = (diseaseName) => {
+    if (!diseasesData) return null;
+    return diseasesData.find((d) => d.name === diseaseName);
+  };
+
+  const diseaseInfo = getDiseaseInfo(disease);
+
+  const handleClick = () => {
+    if (diseaseInfo) {
+      setCost(diseaseInfo.cost);
+      setIsButtonVisible(false);
+    }
+  };
   return (
     <div>
       <div id="recommendWrapper">
@@ -24,6 +44,21 @@ const HosipitalRecommend = () => {
               C병원<br></br>10:00 ~ 18:00<br></br>서울시 마포구 ...<br></br>나와의 거리: 1.3km
             </h1>
           </div>
+          {diseaseInfo && (
+            <div id="averageCost">
+              {isButtonVisible && (
+                <button onClick={handleClick} id="averageCostButton">
+                  평균 진료비용 확인하기
+                </button>
+              )}
+              {cost && (
+                <div>
+                  <h1>{disease} 평균 치료비용:</h1>
+                  <p>{diseaseInfo.cost}</p>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
